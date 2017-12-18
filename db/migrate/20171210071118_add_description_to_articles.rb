08c32646b155565b1879f37bb8e5c250d631a149
Alika Choo <alikachoo5@gmail.com>
2017-12-18 02:52:03 +0000

    I'm stuck here

--- db/migrate/20171210071118_add_description_to_articles.rb
+++ db/migrate/20171210071118_add_description_to_articles.rb
@@ -2,7 +2,7 @@ class AddDescriptionToArticles < ActiveRecord::Migration
   def change
     # Article table, description and type
     add_column :articles, :description, :text
-    add_column :articles, :create_at, :datetime
+    add_column :articles, :created_at, :datetime
     add_column :articles, :updated_at, :datetime
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171216055122) do
+ActiveRecord::Schema.define(version: 20171218023303) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20171216055122) do
     t.datetime "create_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.datetime "created_at"
   end
 
   create_table "users", force: :cascade do |t|
